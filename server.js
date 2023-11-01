e8c98930229643f6d33de37725962c39a5952f09
goswamikeerti <keerti.goswami@imentus.com>
2023-11-01 17:01:07 +0530

    postupload

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 'use strict';
+var cors = require('cors')
+var fileUpload=require('express-fileupload')
+var express = require('express'),
 
-var cors = require('cors');
-require('dotenv').config(); // Load environment variables from .env file
-var express = require('express');
-var app = express(),
+  app = express(),
   port = process.env.PORT || 3000,
-
 
   User = require('./api/models/userModel'),
   bodyParser = require('body-parser'),
@@ -13,25 +12,30 @@ var app = express(),
 
 const mongoose = require('mongoose');
 const option = {
-  socketTimeoutMS: 30000,
-  keepAlive: true,
-  reconnectTries: 30000
+    socketTimeoutMS: 30000,
+    keepAlive: true,
+    reconnectTries: 30000
 };
 
 const mongoURI = process.env.MONGODB_URI;
-mongoose.connect(mongoURI).then(function () {
-  console.log("DB connected successfully")
-}, function (err) {
-  console.log("DB not connected:", err);
+
+const url='mongodb+srv://vikash:root@cluster0.mkrpnfn.mongodb.net/reactnativedb?retryWrites=true&w=majority'
+
+const localUrl='mongodb://127.0.0.1:27017/Property-Suchana';
+
+mongoose.connect(localUrl).then(function(){
+    console.log("DB connected successfully")
+}, function(err) {
+    console.log("DB not connected:", err);
 });
 
+app.use(fileUpload())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors())
-
-app.use(function (req, res, next) {
+app.use(cors());
+app.use(function(req, res, next) {
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function (err, decode) {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
       if (err) req.user = undefined;
       req.user = decode;
       next();
@@ -41,15 +45,20 @@ app.use(function (req, res, next) {
     next();
   }
 });
-var routes = require('./api/routes/userRoutes');
-routes(app);
+var userRoutes = require('./api/routes/userRoutes');
+var postRoutes = require('./api/routes/postRoutes');
 
-app.use(function (req, res) {
+userRoutes(app);
+postRoutes(app);
+
+
+
+app.use(function(req, res) {
   res.status(404).send({ url: req.originalUrl + ' not found' })
 });
 
 app.listen(port);
 
-console.log(' RESTful API server started on: ' + port);
+console.log('RESTful API server started on: ' + port);
 
 module.exports = app;
