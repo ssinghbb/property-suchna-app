d00399f5f9a5d9f4db4580cc1ae20b9af3429ca2
goswamikeerti <keerti.goswami@imentus.com>
2023-11-03 14:25:38 +0530

    save some chage

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -7,8 +7,8 @@ const upload=multer({storage:storage})
 module.exports = function (app) {
   app.route("/post/upload").post(postController.upload);
   app.route("/post/like").put(postController.likePost);
-  app.route("/post/comment").post(postController.addComment);
-  app.route("/post/comments/:postId").get(postController.getComments);
-  app.route("/post/comment/like").put(postController.likeComments);
-  app.route("/post/delete/:commentId").delete(postController.deleteComment);
+  // app.route("/post/comment").post(postController.addComment);
+  // app.route("/post/comments/:postId").get(postController.getComments);
+  // app.route("/post/comment/like").put(postController.likeComments);
+  // app.route("/post/delete/:commentId").delete(postController.deleteComment);
 };
