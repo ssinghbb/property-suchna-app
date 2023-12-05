fd3fe8063da7ebceb60724dff7b87bcf59d094f1
ssinghbb <santosh.singh@imentus.com>
2023-12-05 19:23:15 +0530

    update notification controller

--- api/controllers/notificationController.js
+++ api/controllers/notificationController.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const notifcationModel = require("../models/notificationModel");
+var postSchemaModel = require("../models/postModel");
+
 require("dotenv").config();
 
 
@@ -12,7 +14,20 @@ exports.getNotifications = async function (req, res) {
     console.log("getNotifications contorlller:")
     console.log("req:", req?.params?.userId)
     let _data = await notifcationModel.find({ userId: req?.params?.userId })
-    console.log("_data:", _data)
+    // console.log("_data111:", _data)
+    for (const post of _data) {
+    console.log("post----:", post)
+
+    console.log("post?.id:", post?.postId)
+        const posts = await postSchemaModel.findById(post?.postId)
+        console.log("posts:89098", posts)
+        // console.log("posts:", posts)
+        post.details=posts
+        console.log("post:1111", post)
+    }
+
+    // console.log("_post:", posts)
+    console.log("_data----:", _data)
 
 
     return res.status(200).json(_data);
