a75c696ddc93f33c10519cbec400f8c25910554b
ssinghbb <santosh.singh@imentus.com>
2024-02-22 15:32:31 +0530

    remove auth

--- app.js
+++ app.js
@@ -30,41 +30,42 @@ mongoose.connect(mongoURI).then(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  // console.log(" Bearertoken",req.headers.authorization.split(" ")[0]);
-  // console.log(" maintoken",req.headers.authorization.split(" ")[1]);
-  if (req.originalUrl === '/auth/register' || req.originalUrl === '/auth/verify' || req.originalUrl === '/auth/sign_in' || req.originalUrl === '/testapi') {
-    next(); 
-  } 
-   else if (
-  //  if (
-    req.headers &&
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[0] === "Bearer"
-  ) {
-    try {
-      const isValid = jsonwebtoken.verify(
-        req.headers.authorization.split(" ")[1],
-        process.env.JWT_SECRET_KEY
-      );
-      req.user = isValid.data;
-      next();
-      // res.send({result:"verifyed user"})
-    } catch (error) {
-      console.log("error", error);
-      res.status(401).json({
-        message: "Invalid token",
-        error: error,
-    });
-    }
-  } else {
-    req.user = undefined;
-    res.status(401).json({
-      message: "Unauthorized user",
-  });
-  }
-  // next();
-});
+
+// app.use(function (req, res, next) {
+//   // console.log(" Bearertoken",req.headers.authorization.split(" ")[0]);
+//   // console.log(" maintoken",req.headers.authorization.split(" ")[1]);
+//   if (req.originalUrl === '/auth/register' || req.originalUrl === '/auth/verify' || req.originalUrl === '/auth/sign_in' || req.originalUrl === '/testapi') {
+//     next(); 
+//   } 
+//    else if (
+//   //  if (
+//     req.headers &&
+//     req.headers.authorization &&
+//     req.headers.authorization.split(" ")[0] === "Bearer"
+//   ) {
+//     try {
+//       const isValid = jsonwebtoken.verify(
+//         req.headers.authorization.split(" ")[1],
+//         process.env.JWT_SECRET_KEY
+//       );
+//       req.user = isValid.data;
+//       next();
+//       // res.send({result:"verifyed user"})
+//     } catch (error) {
+//       console.log("error", error);
+//       res.status(401).json({
+//         message: "Invalid token",
+//         error: error,
+//     });
+//     }
+//   } else {
+//     req.user = undefined;
+//     res.status(401).json({
+//       message: "Unauthorized user",
+//   });
+//   }
+//   // next();
+// });
 var userRoutes = require("./api/routes/userRoutes");
 var postRoutes = require("./api/routes/postRoutes");
 var commentRoutes = require("./api/routes/commentRoutes");
